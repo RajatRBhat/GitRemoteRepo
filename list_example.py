ba81f516861eaaa2f87b0a77d136dd3858d4097e
RajatRBhat <rajubhat99@gmail.com>
2018-09-19 22:31:26 +0530

    Append functionality

--- list_example.py
+++ list_example.py
@@ -4,3 +4,16 @@ Created on Sep 19, 2018
 @author: lenovo
 '''
 print("list example")
+list_obj = [1,10,'raj',99.99]
+
+def append(value):
+    list_obj.append(value)
+    return list_obj
+    
+def remove(index):
+    pass
+
+def pop_elem():
+    pass
+    
+print(append(100))
